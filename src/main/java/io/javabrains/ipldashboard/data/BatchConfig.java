acc1f750c27fc79e8a7c838236b8de24e236653c
Pankaj Yadav <nirvanapankaj12@gmail.com>
2021-04-13 17:25:01 +0530

    reverts formatting changes

--- src/main/java/io/javabrains/ipldashboard/data/BatchConfig.java
+++ src/main/java/io/javabrains/ipldashboard/data/BatchConfig.java
@@ -1,6 +1,7 @@
 package io.javabrains.ipldashboard.data;
 
-import io.javabrains.ipldashboard.model.Match;
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -18,15 +19,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import javax.sql.DataSource;
+import io.javabrains.ipldashboard.model.Match;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 
-    private final String[] FIELD_NAMES = new String[]{"id", "city", "date", "player_of_match", "venue",
+    private final String[] FIELD_NAMES = new String[] { "id", "city", "date", "player_of_match", "venue",
             "neutral_venue", "team1", "team2", "toss_winner", "toss_decision", "winner", "result", "result_margin",
-            "eliminator", "method", "umpire1", "umpire2"};
+            "eliminator", "method", "umpire1", "umpire2" };
 
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
@@ -54,32 +55,30 @@ public class BatchConfig {
     public JdbcBatchItemWriter<Match> writer(DataSource dataSource) {
         return new JdbcBatchItemWriterBuilder<Match>()
                 .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
-                .sql("INSERT INTO match (id, city, date, player_of_match, venue, team1, team2, toss_winner, toss_decision, match_winner, result, " +
-                        "result_margin, umpire1, umpire2) "
-                        + " VALUES (:id, :city, :date, :playerOfMatch, :venue, :team1, :team2, :tossWinner, :tossDecision, :matchWinner, :result, " +
-                        ":resultMargin, :umpire1, :umpire2)")
+                .sql("INSERT INTO match (id, city, date, player_of_match, venue, team1, team2, toss_winner, toss_decision, match_winner, result, result_margin, umpire1, umpire2) "
+                        + " VALUES (:id, :city, :date, :playerOfMatch, :venue, :team1, :team2, :tossWinner, :tossDecision, :matchWinner, :result, :resultMargin, :umpire1, :umpire2)")
                 .dataSource(dataSource).build();
     }
 
     @Bean
     public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
         return jobBuilderFactory
-                .get("importUserJob")
-                .incrementer(new RunIdIncrementer())
-                .listener(listener)
-                .flow(step1)
-                .end()
-                .build();
+            .get("importUserJob")
+            .incrementer(new RunIdIncrementer())
+            .listener(listener)
+            .flow(step1)
+            .end()
+            .build();
     }
 
     @Bean
     public Step step1(JdbcBatchItemWriter<Match> writer) {
         return stepBuilderFactory
-                .get("step1")
-                .<MatchInput, Match>chunk(10)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer)
-                .build();
+            .get("step1")
+            .<MatchInput, Match>chunk(10)
+            .reader(reader())
+            .processor(processor())
+            .writer(writer)
+            .build();
     }
 }
--- src/main/java/io/javabrains/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/io/javabrains/ipldashboard/data/JobCompletionNotificationListener.java
@@ -1,6 +1,11 @@
 package io.javabrains.ipldashboard.data;
 
-import io.javabrains.ipldashboard.model.Team;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
@@ -9,10 +14,7 @@ import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
-import java.util.HashMap;
-import java.util.Map;
+import io.javabrains.ipldashboard.model.Team;
 
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
@@ -26,38 +28,38 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
         this.em = em;
     }
 
-    @Override
-    @Transactional
-    public void afterJob(JobExecution jobExecution) {
-        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-            log.info("!!! JOB FINISHED! Time to verify the results");
+  @Override
+  @Transactional
+  public void afterJob(JobExecution jobExecution) {
+    if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+      log.info("!!! JOB FINISHED! Time to verify the results");
+      
+      Map<String, Team> teamData = new HashMap<>();
+            
+      em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
+        .getResultList()
+        .stream()
+        .map(e -> new Team((String) e[0], (long) e[1]))
+        .forEach(team -> teamData.put(team.getTeamName(), team));
+    
+        em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
+        .getResultList()
+        .stream()
+        .forEach(e -> {
+            Team team = teamData.get((String) e[0]);
+            team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
+        });
 
-            Map<String, Team> teamData = new HashMap<>();
+        em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
+        .getResultList()
+        .stream()
+        .forEach(e -> {
+            Team team = teamData.get((String) e[0]);
+            if (team != null) team.setTotalWins((long) e[1]);
+        });
 
-            em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
-                    .getResultList()
-                    .stream()
-                    .map(e -> new Team((String) e[0], (long) e[1]))
-                    .forEach(team -> teamData.put(team.getTeamName(), team));
-
-            em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
-                    .getResultList()
-                    .stream()
-                    .forEach(e -> {
-                        Team team = teamData.get((String) e[0]);
-                        team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
-                    });
-
-            em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
-                    .getResultList()
-                    .stream()
-                    .forEach(e -> {
-                        Team team = teamData.get((String) e[0]);
-                        if (team != null) team.setTotalWins((long) e[1]);
-                    });
-
-            teamData.values().forEach(team -> em.persist(team));
-            teamData.values().forEach(team -> System.out.println(team));
-        }
+        teamData.values().forEach(team -> em.persist(team));
+        teamData.values().forEach(team -> System.out.println(team));
     }
+  }
 }
