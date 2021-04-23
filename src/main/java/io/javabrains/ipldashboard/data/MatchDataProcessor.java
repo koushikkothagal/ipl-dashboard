95c76ead2f607f6bcc8b101b98eda83fda2763ee
yahitesh <hiteshyadav1992@gmail.com>
2021-04-23 10:16:28 +0530

    Use map struct for mapping

--- src/main/java/io/javabrains/ipldashboard/data/MatchDataProcessor.java
+++ src/main/java/io/javabrains/ipldashboard/data/MatchDataProcessor.java
@@ -16,39 +16,7 @@ public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
 
     @Override
     public Match process(final MatchInput matchInput) throws Exception {
-        
-        Match match = new Match();
-        match.setId(Long.parseLong(matchInput.getId()));
-        match.setCity(matchInput.getCity());
-
-        match.setDate(LocalDate.parse(matchInput.getDate()));
-
-        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
-        match.setVenue(matchInput.getVenue());
-
-        // Set Team 1 and Team 2 depending on the innings order
-        String firstInningsTeam, secondInningsTeam;
-
-        if ("bat".equals(matchInput.getToss_decision())) {
-            firstInningsTeam = matchInput.getToss_winner();
-            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) 
-                ? matchInput.getTeam2() : matchInput.getTeam1();
-
-        } else {
-            secondInningsTeam = matchInput.getToss_winner();
-            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) 
-                ? matchInput.getTeam2() : matchInput.getTeam1();
-        }
-        match.setTeam1(firstInningsTeam);
-        match.setTeam2(secondInningsTeam);
-
-        match.setTossWinner(matchInput.getToss_winner());
-        match.setTossDecision(matchInput.getToss_decision());
-        match.setMatchWinner(matchInput.getWinner());
-        match.setResult(matchInput.getResult());
-        match.setResultMargin(matchInput.getResult_margin());
-        match.setUmpire1(matchInput.getUmpire1());
-        match.setUmpire2(matchInput.getUmpire2());
+        Match match = MatchMapper.INSTANCE.toMatch(matchInput);
 
         return match;
     }
---
+++ src/main/java/io/javabrains/ipldashboard/data/MatchMapper.java
@@ -0,0 +1,59 @@
+package io.javabrains.ipldashboard.data;
+
+import java.time.LocalDate;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+import io.javabrains.ipldashboard.model.Match;
+
+@Mapper(componentModel="spring")
+public interface MatchMapper {
+    public static final MatchMapper INSTANCE = Mappers.getMapper(MatchMapper.class);
+
+    @Mappings({ @Mapping(source = "date", target = "date", qualifiedByName = "dateConvert"),
+            @Mapping(source = "player_of_match", target = "playerOfMatch"),
+            @Mapping(source = "matchInput", target = "team1", qualifiedByName = "firstInningsTeam"),
+             @Mapping(source = "matchInput", target = "team2", qualifiedByName = "secondInningsTeam"),
+            @Mapping(source = "toss_winner", target = "tossWinner"),
+            @Mapping(source = "toss_decision", target = "tossDecision"),
+            @Mapping(source = "winner", target = "matchWinner"),
+            @Mapping(source = "result_margin", target = "resultMargin") 
+        })
+    public Match toMatch(MatchInput matchInput);
+
+    @Named("dateConvert")
+    default LocalDate dateConvert(String date) {
+        return LocalDate.parse(date);
+    }
+
+    @Named("firstInningsTeam")
+    default String firstInningsTeam(MatchInput matchInput) {
+        String firstInningsTeam;
+        if ("bat".equals(matchInput.getToss_decision())) {
+            firstInningsTeam = matchInput.getToss_winner();
+
+        } else {
+            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ? matchInput.getTeam2()
+                    : matchInput.getTeam1();
+        }
+        return firstInningsTeam;
+    }
+
+    @Named("secondInningsTeam")
+    default String secondInningsTeam(MatchInput matchInput) {
+        String secondInningsTeam;
+
+        if ("bat".equals(matchInput.getToss_decision())) {
+            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ? matchInput.getTeam2()
+                    : matchInput.getTeam1();
+
+        } else {
+            secondInningsTeam = matchInput.getToss_winner();
+        }
+        return secondInningsTeam;
+    }
+}
