d115e9efc00e9271bb400c33e3718e1f930927f2
Kashish Durgiya <kdurgiya@gmail.com>
2021-05-19 22:48:11 +0530

    removing spacing added

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -29,19 +29,19 @@ export const MatchPage = () => {
    
     return (
 		<div>
-			<Link to="/">Home</Link>
-	        <div className="MatchPage">
-	            <div className="year-selector">
-	                <h3> Select Year </h3>
-	                <YearSelector teamName={teamName} />
-	            </div>
-	            <div>
-	                <h1 className="page-heading">{teamName} matches in {year}</h1>
-	                {
-	                    matches.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match} />)
-	                }
-	            </div>
-			</div>
+		<Link to="/">Home</Link>
+        <div className="MatchPage">
+            <div className="year-selector">
+                <h3> Select Year </h3>
+                <YearSelector teamName={teamName} />
+            </div>
+            <div>
+                <h1 className="page-heading">{teamName} matches in {year}</h1>
+                {
+                    matches.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match} />)
+                }
+            </div>
+		</div>
         </div>
     );
 }
